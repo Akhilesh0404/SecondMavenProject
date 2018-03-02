d7f6d01d72a4d65f2ec730e35c25afae5d21edfc
akhilesh <akhilesh@akki>
2018-03-02 20:38:43 +0530

    First Commit

---
+++ SecondMavenProject/src/test/java/secondTestPackage/SecondTestnG.java
@@ -0,0 +1,48 @@
+package secondTestPackage;
+
+import org.testng.annotations.Test;
+
+public class SecondTestnG {
+
+@Test
+public void TestFireFox(){
+	
+	System.out.println("Welcome test Ng7");
+	System.out.println("Welcome test Ng8");
+	System.out.println("Welcome test Ng9");
+	System.out.println("Welcome test Ng10");
+	System.out.println("Welcome test Ng11");
+	System.out.println("Welcome test Ng12");
+
+/*WebDriver driver=new FirefoxDriver();
+
+driver.manage().window().maximize();
+
+driver.get("http://www.facebook.com");
+
+driver.quit();
+*/
+}
+
+
+
+@Test
+public void TestFireFox1(){
+	
+	System.out.println("Welcome test Ng1");
+	System.out.println("Welcome test Ng2");
+	System.out.println("Welcome test Ng3");
+	System.out.println("Welcome test Ng4");
+	System.out.println("Welcome test Ng5");
+	System.out.println("Welcome test Ng6");
+
+/*WebDriver driver=new FirefoxDriver();
+
+driver.manage().window().maximize();
+
+driver.get("http://www.facebook.com");
+
+driver.quit();
+*/
+}
+}
